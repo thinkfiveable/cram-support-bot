d71304e1ce5a82bc42539060075cbdd44daf825a
Zaid "Nico <me@zaid-arshad.tech>
2021-05-10 10:33:33 -0400

    feat: fetch command

---
+++ src/commands/fetch.ts
@@ -0,0 +1,53 @@
+import { stripIndents } from 'common-tags';
+import { Command } from 'discord-akairo';
+import { User, Collection, Message } from 'discord.js';
+import Thread from '../schemas/Thread';
+
+export default class Fetch extends Command {
+	public constructor() {
+		super('fetch', {
+			aliases: ['fetch'],
+			ratelimit: 2,
+			cooldown: 20000,
+			args: [
+				{
+					id: 'id',
+					type: 'string',
+				},
+			],
+			channel: 'guild',
+		});
+	}
+
+	public async exec(msg: Message, { id }: { id: string }) {
+		const fetch_thread = await Thread.findById(id);
+		if (!fetch_thread) return msg.channel.send('Thread with that ID does not exist!');
+
+		const opener = await this.client.users.fetch(fetch_thread.author_id);
+		const responderIDs = new Set(fetch_thread.messages.map((x) => x.msg_author_id));
+		const responders = new Collection<string, User | null>();
+
+		responders.set(opener.id, opener);
+		responderIDs.forEach(async (responder) => {
+			if (responder === opener.id) return;
+			responders.set(responder, await this.client.users.fetch(responder).catch(() => null));
+		});
+
+		const message_log = fetch_thread.messages
+			.map((x) => `\`${responders.get(x.msg_author_id)?.tag ?? 'UNKNOWN'}: ${x.content}\``)
+			.join('\n');
+
+		return msg.channel.send(stripIndents`
+            **ID:** \`${msg.id}\`
+			**Opener:** \`${opener.tag} (${opener.id})\`
+			**Issue:** \`${
+				fetch_thread.data.issue.length > 300 ? `${fetch_thread.data.issue.substring(0, 300)}...` : fetch_thread
+			}\`
+			**Responders:** \`${responders.map((x, id) => (x ? `\`${x.tag} (${x.id})\`` : `\`UNKNOWN (${id})\``))}\`
+			**Messages:**
+
+			${message_log.length > 1200 ? `${message_log.substring(0, 1200)}...` : message_log}
+            `);
+	}
+}
+//
