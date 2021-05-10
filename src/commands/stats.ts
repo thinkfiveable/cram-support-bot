d34b20f918cd1e4f26c11ec360f73d899c108a7c
Zaid "Nico <me@zaid-arshad.tech>
2021-05-10 14:44:56 -0400

    feat: stats command

---
+++ src/commands/stats.ts
@@ -0,0 +1,47 @@
+import { stripIndents } from 'common-tags';
+import { Command } from 'discord-akairo';
+import { MessageEmbed, Message } from 'discord.js';
+
+export default class Stats extends Command {
+	public constructor() {
+		super('stats', {
+			aliases: ['stats', 'stat', 'infoo'],
+			ratelimit: 2,
+			cooldown: 20000,
+			args: [],
+			channel: 'guild',
+			description: {
+				content: 'Get statistics about this running instance.',
+				usage: [''],
+				example: [''],
+			},
+		});
+	}
+
+	public async exec(msg: Message) {
+		const creator = this.client.users.cache.get(this.client.config.ADMIN_ID);
+		// inspiration from https://github.com/Naval-Base/yuudachi/blob/master/src/bot/commands/util/stats.ts
+		return msg.channel.send(
+			new MessageEmbed()
+				.setTitle('Statistics about me!')
+				.setColor('GREEN')
+				.addField('❯ Memory Usage', `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)}MB`, true)
+				.addField(
+					'❯ General Stats',
+					stripIndents`
+                    • Users: ${this.client.users.cache.size}
+                    • Channels: ${this.client.channels.cache.size}
+                `,
+					true,
+				)
+				.addField('❯ Source Code', '[View Here](https://github.com/ThinkFiveable/cram-support-bot)', false)
+				.addField(
+					'❯ Library',
+					'[discord.js](https://discord.js.org)[-akairo](https://github.com/1Computer1/discord-akairo)',
+					true,
+				)
+				.setThumbnail(this.client.user?.displayAvatarURL() ?? '')
+				.setFooter(`© 2021 Fiveable | Created by ${creator!.tag}`, creator!.displayAvatarURL()),
+		);
+	}
+}
